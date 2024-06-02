8f18c5b49c9c184c8cf29ee3bff3fef42c9bbcf1
Arthur Torres <arthurtorres75@gmail.com>
2024-06-01 23:33:23 -0400

    adding users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ProductModule,
     MongooseModule.forRoot('mongodb://localhost/products'),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
