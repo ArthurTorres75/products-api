06dd8eefca122fc62c2f3d2afd9356e48d526e14
Arthur Torres <arthurtorres75@gmail.com>
2024-06-04 11:43:04 -0400

    adding users crud in the controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserDto } from './dto/user.dto';
 import { Response } from 'express';
@@ -19,5 +30,34 @@ export class UsersController {
   async getUsers(@Res() res: Response) {
     const users = await this.userService.getUsers();
     return res.status(HttpStatus.OK).json(users);
+  }
+
+  @Get('/:userId')
+  async getUser(@Res() res: Response, @Param('userId') userId: string) {
+    const user = await this.userService.getUser(userId);
+    if (!user) throw new NotFoundException('User not found');
+    return res.status(HttpStatus.OK).json(user);
+  }
+
+  @Delete(':userId')
+  async deleteUSer(@Res() res: Response, @Param('userId') userId: string) {
+    const deletedUser = await this.userService.deleteUser(userId);
+    if (!deletedUser) throw new NotFoundException('User not found');
+    return res
+      .status(HttpStatus.OK)
+      .json({ message: 'User deleted successfully', deletedUser });
+  }
+
+  @Put('/update/:userId')
+  async updateUser(
+    @Res() res: Response,
+    @Param('userId') userId: string,
+    @Body() userDto: UserDto,
+  ) {
+    const updatedUser = await this.userService.updateUser(userId, userDto);
+    if (!updatedUser) throw new NotFoundException('User not found');
+    return res
+      .status(HttpStatus.OK)
+      .json({ message: 'user updated successfully', updatedUser });
   }
 }
